2c89e5eb5fb0d75706eb29da1db52269d3a8d4f3
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2025-03-19 18:17:39 +0200

    hi

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function Home() {
   return <>
-    <h1 className='text-center text-white'>******Home******</h1>
+    <h1 className='text-center text-white'>assdsadsadasdsa******Home******ffffffffffff</h1>
   </>
   
 }
