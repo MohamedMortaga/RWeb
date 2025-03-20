2dd681eb07f45075bc3dd116ebb05f102fcfa0b6
Andrew Melad Makram <92456025+Andrew-120@users.noreply.github.com>
2025-03-20 15:07:35 +0200

    Team Updated

--- src/Component/Team/Team.jsx
+++ src/Component/Team/Team.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './Team.module.css'
 import { FaPeopleGroup } from "react-icons/fa6";
+import axios from 'axios';
+import 'animate.css';
 
 export default function Team() {
+  const [team, setTeam] = useState([]);
+
+  async function getTeam() {
+    let {data} = await axios.get('https://api.sampleapis.com/presidents/presidents')
+    console.log(data);
+    setTeam(data)
+  }
+
+  useEffect(() => {
+    getTeam()
+  }, [])
+
   return <>
-  <section className='vh-100'>
+  <section className='vh-100 overflow-auto'>
   <fieldset className={`${style.fs1} ${style.borderr} p-3`}>
           <h2 className="text-dark">
             <FaPeopleGroup style={{ verticalAlign: 'middle', marginRight: '10px' }} /> Our Team
           </h2>
-        </fieldset>
+
+          <div className="container animate__animated animate__zoomIn delay-1s">
+            <div className="row">
+              {team.map((item) =><div className="col-md-3" key={item.id}>
+                <img src={item.photo} alt={item.name} className='img-fluid w-100 rounded rounded-4 border border-3 border-primary mt-2' style={{ height: '450px', objectFit: 'cover' }} />
+                <h3 className='text-center mt-1'>{item.name}</h3>
+                </div>)}
+            </div>
+          </div>
+  </fieldset>
   </section>
  
   </>
