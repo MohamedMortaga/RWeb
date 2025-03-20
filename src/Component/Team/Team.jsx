c71ce7f65ce05fd68cfa24bf90ca56a8fe1e96b0
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2025-03-20 16:38:31 +0200

    new

--- src/Component/Team/Team.jsx
+++ src/Component/Team/Team.jsx
@@ -18,19 +18,18 @@ export default function Team() {
   }, [])
 
   return <>
-  <section className='vh-100 overflow-auto'>
-  <fieldset className={`${style.fs1} ${style.borderr} p-3`}>
+  <section className='p-5'>
+  <fieldset className={`${style.fs1} ${style.borderr} p-3  m-5`}>
           <h2 className="text-dark">
             <FaPeopleGroup style={{ verticalAlign: 'middle', marginRight: '10px' }} /> Our Team
           </h2>
 
           <div className="container animate__animated animate__zoomIn delay-1s">
             <div className="row">
-              {team.map((item) =><div className="col-md-3" key={item.id}>
+              {team.slice(0, 4).map((item) => <div className="col-md-3" key={item.id}>
                 <img src={item.photo} alt={item.name} className='img-fluid w-100 rounded rounded-4 border border-3 border-primary mt-2' style={{ height: '450px', objectFit: 'cover' }} />
-                <h3 className='text-center mt-1'>{item.name}</h3>
-                <p className='text-center text-primary'>{item.yearsInOffice}</p>
-                </div>)}
+
+                <h3 className='text-center mt-1'>{item.name}</h3> <p className='text-center text-primary'>{item.yearsInOffice}</p> </div>)}
             </div>
           </div>
   </fieldset>
