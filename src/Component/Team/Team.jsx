055bff2629abbc8f45c74a817cc2689ec116d6ac
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2025-03-20 13:46:52 +0200

    again

--- src/Component/Team/Team.jsx
+++ src/Component/Team/Team.jsx
@@ -5,7 +5,7 @@ import { FaPeopleGroup } from "react-icons/fa6";
 export default function Team() {
   return <>
   <section className='vh-100'>
-  <fieldset className={`${style.fs1} ${style.borderr} p-3`}>
+  <fieldset className={`${style.fs1} ${style.borderr} p-3 m-5`}>
           <h2 className="text-dark">
             <FaPeopleGroup style={{ verticalAlign: 'middle', marginRight: '10px' }} /> Our Team
           </h2>
